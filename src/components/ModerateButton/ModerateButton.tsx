c6a37162b4cd12b31334ee301dca72f0fdeb1d2a
blonde-mike <macrowther7+git@gmail.com>
2022-05-05 14:07:40 -0600

    Pausing to pull

--- src/components/ModerateButton/ModerateButton.tsx
+++ src/components/ModerateButton/ModerateButton.tsx
@@ -1,14 +1,64 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { Button, Dialog, DialogActions, DialogTitle, DialogContent, Typography } from '@mui/material'
 
+interface ModerateButtonProps {
+  cisId: string,
+  conclusionType: string,
+  personId: string
+}
 
-interface ModerateButtonProps {}
+const ModerateButton: FC<ModerateButtonProps> = ({cisId, conclusionType, personId}) => {
+  const [openModerateNotice, setOpenModerateNotice] = useState(false);
 
-const ModerateButton: FC<ModerateButtonProps> = () => {
+  const handleModerateClick = () => {
+    setOpenModerateNotice(true);
+  }
+
+  const handleCloseModerateDialog = () => {
+    setOpenModerateNotice(false);
+  }
+
+  const handleConfirmModeration = () => {
+    // let userKey = findLocalItems(/prod-v2-user/);
+    console.log(cisId);
+    console.log(conclusionType);
+    console.log(personId);
+    // localStorage.getItem()
+    setOpenModerateNotice(false);
+  }
   
   return (
-    <button className="edit-button fs-button fs-button--minor fs-button--small">
-      Moderate
-    </button>
+    <span>
+      <Button className="edit-button fs-button fs-button--minor fs-button--small"
+      onClick={handleModerateClick}>
+        Moderate
+      </Button>
+      <Dialog onClose={handleCloseModerateDialog}
+      open={openModerateNotice}
+      sx={{
+        width: 1500
+      }}>
+        <DialogTitle variant="h4">
+          Moderating This Event
+        </DialogTitle>
+        <DialogContent>
+          <Typography variant="body1">
+            By clicking "Confirm", you agree to follow the Community Guidelines for moderation. Please be courteous and respectful in your interactions with other users.
+            
+            Thank you for moderating!
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained"
+          onClick={handleConfirmModeration}>Confirm</Button>
+          <Button variant="outlined"
+          onClick={handleCloseModerateDialog}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </span>
+    
   )
 };
 
