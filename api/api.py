2db9c078a44ccb7bddaa0677a91f2cd4dfb7da1f
John Sumsion <sumsionjg@familysearch.org>
2022-05-07 00:37:28 -0600

    Implement proposed as an object containing a list of conclusions

--- api/api.py
+++ api/api.py
@@ -18,84 +18,94 @@ data = {
                 "userId": "cis.user.M7X1-R1ZT",
                 "contactName": "blondemike",
             },
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "sex": {
             "moderator": {
                 "userId": "cis.user.M7X1-R1ZT",
                 "contactName": "blondemike",
             },
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "birth": {
             "moderator": {
                 "userId": "cis.user.M7X1-R1ZT",
                 "contactName": "blondemike",
             },
-            "proposed": [
-                {
-                "userId": "cis.user.M7X1-R1ZB",
-                "contactName": "testman77",
-                "conclusion": {
-                    "date": "13 September 1903",
-                    "place": "Colonia Juárez, Casas Grandes, Chihuahua, Mexico",
-                    "contributor": {
-                      "date": "June 5, 2021",
-                      "id": "MM7W-8GC",
-                      "name": "JB Cowley",
-                      "timestamp": "2021-06-05T15:08:17+00:00"
-                    },
-                    "details": {
-                      "detailsType": "EventDetails",
-                      "date": {
-                        "formalText": "+1903-09-13",
-                        "julianDateRange": {
-                          "earliestDay": 2416371,
-                          "latestDay": 2416371
-                        },
-                        "localizedText": "13 September 1903",
-                        "modifier": null,
-                        "normalizedText": "13 September 1903",
-                        "originalText": "13 September 1903"
-                      },
-                      "deceasedFlag": null,
-                      "description": null,
-                      "place": {
-                        "geoCode": {
-                          "latitude": 30.3098,
-                          "longitude": -108.0785
-                        },
-                        "id": 6902982,
-                        "localizedText": "Colonia Juárez, Casas Grandes, Chihuahua, Mexico",
-                        "normalizedText": "Colonia Juárez, Casas Grandes, Chihuahua, Mexico",
-                        "originalText": "Colonia Juárez, Casas Grandes, Chihuahua, Mexico"
-                      },
-                      "sourceCount": 14,
-                      "title": "",
-                      "type": "BIRTH"
-                    },
-                    "id": "248eaba5-34ae-4182-9874-bfcc07166241",
-                    "justification": "As reported in her obituary, Salt Lake Tribune, 8 September 1997.",
-                    "multiValued": false,
-                    "type": "BIRTH"
-                  }
-            }
-            ]
+            "proposed": {
+                "conclusions": [
+                    {
+                        "userId": "cis.user.M7X1-R1ZB",
+                        "contactName": "blondemike2",
+                        "conclusion": {
+                          "contributor": {
+                            "date": "June 5, 2021",
+                            "id": "MM7W-8GC",
+                            "name": "JB Cowley",
+                            "timestamp": "2021-06-05T15:08:17+00:00"
+                          },
+                          "details": {
+                            "detailsType": "EventDetails",
+                            "date": {
+                              "formalText": "+1903-09-13",
+                              "julianDateRange": {
+                                "earliestDay": 2416371,
+                                "latestDay": 2416371
+                              },
+                              "localizedText": "13 September 1903",
+                              "modifier": null,
+                              "normalizedText": "13 September 1903",
+                              "originalText": "13 September 1903"
+                            },
+                            "deceasedFlag": null,
+                            "description": null,
+                            "place": {
+                              "geoCode": {
+                                "latitude": 30.3098,
+                                "longitude": -108.0785
+                              },
+                              "id": 6902982,
+                              "localizedText": "Colonia Juárez, Casas Grandes, Chihuahua, Mexico",
+                              "normalizedText": "Colonia Juárez, Casas Grandes, Chihuahua, Mexico",
+                              "originalText": "Colonia Juárez, Casas Grandes, Chihuahua, Mexico"
+                            },
+                            "sourceCount": 14,
+                            "title": "",
+                            "type": "BIRTH"
+                          },
+                          "id": "248eaba5-34ae-4182-9874-bfcc07166241",
+                          "justification": "As reported in her obituary, Salt Lake Tribune, 8 September 1997.",
+                          "multiValued": false,
+                          "type": "BIRTH"
+                        }
+                    }
+                ],
+            },
         },
         "christening": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "death": {
             "moderator": {
                 "userId": "cis.user.M7X1-R1ZT",
                 "contactName": "blondemike",
             },
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "burial": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
     },
 }
@@ -123,16 +133,18 @@ def data_for_person_slot_proposed(pid, slot):
 @app.route('/api/<pid>/<slot>/moderator', methods=['POST', 'PUT'])
 def data_for_person_slot_moderator_POST(pid, slot):
     required_attrs = [ "userId", "contactName" ]
-    post_attr = "moderator"
-    return verify_and_post(data, pid, slot, required_attrs, post_attr)
+    def assign_moderator(slot_data, post_data):
+        slot_data["moderator"] = post_data
+    return verify_and_post(data, pid, slot, required_attrs, "moderator", assign_moderator)
 
 @app.route('/api/<pid>/<slot>/proposed', methods=['POST', 'PUT'])
 def data_for_person_slot_proposed_POST(pid, slot):
     required_attrs = [ "userId", "contactName", "conclusion" ]
-    post_attr = "proposed"
-    return verify_and_post(data, pid, slot, required_attrs, post_attr)
+    def assign_proposed(slot_data, post_data):
+        slot_data["proposed"]["conclusions"] += [ post_data ]
+    return verify_and_post(data, pid, slot, required_attrs, "proposed", assign_proposed)
 
-def verify_and_post(data, pid, slot, required_attrs, post_attr):
+def verify_and_post(data, pid, slot, required_attrs, post_attr, assign_lambda):
     # make sure there is a proper application/json body
     if not request.json:
         abort(400)
@@ -146,7 +158,7 @@ def verify_and_post(data, pid, slot, required_attrs, post_attr):
     post_data = request.json
     for required_attr in required_attrs:
         verify_attribute(post_data, required_attr)
-    slot_data[post_attr] = post_data
+    assign_lambda(slot_data, post_data)
     # re-traverse to make sure the data is updated properly, also send CORS header for localhost access
     return jsonify(traverse(data, pid, slot, post_attr))
 
@@ -167,27 +179,39 @@ def stub_person(data, pid):
     person_data = {
         "name": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "sex": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "birth": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "christening": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "death": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
         "burial": {
             "moderator": null,
-            "proposed": null,
+            "proposed": {
+                "conclusions": [],
+            },
         },
     }
     data[pid] = person_data
